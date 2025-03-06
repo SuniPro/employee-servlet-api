e9905bfa4b6a0c93dcffae4f8d3b9f4ead2659a8
Ariel-250225 <seobin250225@icloud.com>
2025-03-06 20:06:09 +0700

    feature : project build

---
+++ src/main/java/com/taekang/employeeservletapi/EmployeeServletApiApplication.java
@@ -0,0 +1,13 @@
+package com.taekang.employeeservletapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeServletApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeServletApiApplication.class, args);
+    }
+
+}

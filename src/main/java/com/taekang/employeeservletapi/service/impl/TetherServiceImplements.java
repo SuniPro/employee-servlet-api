20f787266e9016034242219402fc7c8948e60970
Ariel-250225 <seobin250225@icloud.com>
2025-04-16 21:06:36 +0700

    refactor : 불필요 파일 삭제, 미사용 import 제거

--- src/main/java/com/taekang/employeeservletapi/service/impl/TetherServiceImplements.java
+++ src/main/java/com/taekang/employeeservletapi/service/impl/TetherServiceImplements.java
@@ -10,7 +10,6 @@ import com.taekang.employeeservletapi.error.DepositNotFoundOrAlreadyApprovedExce
 import com.taekang.employeeservletapi.error.DepositVerificationException;
 import com.taekang.employeeservletapi.repository.user.TetherAccountRepository;
 import com.taekang.employeeservletapi.repository.user.TetherDepositRepository;
-import com.taekang.employeeservletapi.repository.user.TetherWithdrawRepository;
 import com.taekang.employeeservletapi.service.TetherService;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -23,135 +22,149 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class TetherServiceImplements implements TetherService {
 
-    private final TetherAccountRepository tetherAccountRepository;
+  private final TetherAccountRepository tetherAccountRepository;
 
-    private final TetherDepositRepository tetherDepositRepository;
+  private final TetherDepositRepository tetherDepositRepository;
 
-    private final TetherWithdrawRepository tetherWithdrawRepository;
+  @Autowired
+  public TetherServiceImplements(
+      TetherAccountRepository tetherAccountRepository,
+      TetherDepositRepository tetherDepositRepository) {
+    this.tetherAccountRepository = tetherAccountRepository;
+    this.tetherDepositRepository = tetherDepositRepository;
+  }
 
-    @Autowired
-    public TetherServiceImplements(TetherAccountRepository tetherAccountRepository, TetherDepositRepository tetherDepositRepository, TetherWithdrawRepository tetherWithdrawRepository) {
-        this.tetherAccountRepository = tetherAccountRepository;
-        this.tetherDepositRepository = tetherDepositRepository;
-        this.tetherWithdrawRepository = tetherWithdrawRepository;
+  /** 모든 입금 내역을 Id를 기준으로 조회합니다. */
+  @Override
+  @Transactional
+  public TetherAccount updateTetherWallet(String tetherWallet) {
+    TetherAccount tetherAccount =
+        tetherAccountRepository
+            .findByTetherWallet(tetherWallet)
+            .orElseThrow(AccountNotFoundException::new);
+
+    tetherAccount =
+        tetherAccount.toBuilder()
+            .tetherWallet(tetherWallet)
+            .updateDateTime(LocalDateTime.now(ZoneId.of("Asia/Seoul")))
+            .build();
+    return tetherAccountRepository.save(tetherAccount);
+  }
+
+  /** 입금 내역을 승인합니다. */
+  @Override
+  @Transactional
+  public Boolean depositAccept(TetherDepositAcceptedDTO tetherDepositAcceptedDTO) {
+    TetherAccount tetherAccount =
+        tetherAccountRepository
+            .findByTetherWallet(tetherDepositAcceptedDTO.getTetherWallet())
+            .orElseThrow(AccountNotFoundException::new);
+
+    // 2. 기존 입금 요청 확인
+    TetherDeposit tetherDeposit =
+        tetherDepositRepository
+            .findById(tetherDepositAcceptedDTO.getDepositId())
+            .orElseThrow(DepositNotFoundOrAlreadyApprovedException::new);
+
+    // 3. 검증: 계정 & 금액 일치 여부 확인
+    if (!tetherDeposit.getTetherAccount().equals(tetherAccount)
+        || tetherDeposit.getAmount().compareTo(tetherDepositAcceptedDTO.getAmount()) != 0) {
+      throw new DepositVerificationException(); // 커스텀 예외로 명확히
     }
 
-    /** 모든 입금 내역을 Id를 기준으로 조회합니다. */
-    @Override
-    @Transactional
-    public TetherAccount updateTetherWallet(String tetherWallet) {
-        TetherAccount tetherAccount = tetherAccountRepository.findByTetherWallet(tetherWallet).orElseThrow(AccountNotFoundException::new);
+    // 4. 상태 변경
+    TetherDeposit updated =
+        tetherDeposit.toBuilder()
+            .status(TransactionStatus.CONFIRMED)
+            .accepted(true)
+            .acceptedAt(LocalDateTime.now(ZoneId.of("Asia/Seoul")))
+            .build();
 
-        tetherAccount = tetherAccount.toBuilder()
-                .tetherWallet(tetherWallet)
-                .updateDateTime(LocalDateTime.now(ZoneId.of("Asia/Seoul")))
-                .build();
-        return tetherAccountRepository.save(tetherAccount);
-    }
+    tetherDepositRepository.save(updated);
+    return updated.getAccepted();
+  }
 
-    /** 입금 내역을 승인합니다. */
-    @Override
-    @Transactional
-    public Boolean depositAccept(TetherDepositAcceptedDTO tetherDepositAcceptedDTO) {
-        TetherAccount tetherAccount = tetherAccountRepository.findByTetherWallet(tetherDepositAcceptedDTO.getTetherWallet())
-                .orElseThrow(AccountNotFoundException::new);
+  @Override
+  @Transactional
+  public Boolean withdrawAccept(String tetherWallet, BigDecimal amount) {
+    return null;
+  }
 
-        // 2. 기존 입금 요청 확인
-        TetherDeposit tetherDeposit = tetherDepositRepository.findById(tetherDepositAcceptedDTO.getDepositId())
-                .orElseThrow(DepositNotFoundOrAlreadyApprovedException::new);
+  /** 모든 입금 내역을 Id를 기준으로 조회합니다. */
+  @Override
+  @Transactional(readOnly = true)
+  public List<TetherDeposit> getDepositsForAccount(Long accountId) {
+    return tetherDepositRepository.findByTetherAccount_IdOrderByRequestedAtDesc(accountId);
+  }
 
-        // 3. 검증: 계정 & 금액 일치 여부 확인
-        if (!tetherDeposit.getTetherAccount().equals(tetherAccount) ||
-            tetherDeposit.getAmount().compareTo(tetherDepositAcceptedDTO.getAmount()) != 0) {
-            throw new DepositVerificationException(); // 커스텀 예외로 명확히
-        }
+  /** 모든 입금내역을 상태를 기준으로 조회합니다. */
+  @Override
+  @Transactional(readOnly = true)
+  public List<TetherDeposit> getDepositsByStatus(TransactionStatus status) {
+    return tetherDepositRepository.findByStatus(status);
+  }
 
-        // 4. 상태 변경
-        TetherDeposit updated = tetherDeposit.toBuilder()
-                .status(TransactionStatus.CONFIRMED)
-                .accepted(true)
-                .acceptedAt(LocalDateTime.now(ZoneId.of("Asia/Seoul")))
-                .build();
+  /** 아이디를 기준으로 특정 상태의 입금을 조회합니다. */
+  @Override
+  @Transactional(readOnly = true)
+  public List<TetherDeposit> getApprovedDepositsForAccountById(
+      Long accountId, TransactionStatus status) {
+    return tetherDepositRepository.findByTetherAccount_IdAndStatus(accountId, status);
+  }
 
-        tetherDepositRepository.save(updated);
-        return updated.getAccepted();
-    }
+  /** 승인된 모든 입금내역을 특정 지갑을 기준으로 조회합니다. */
+  @Override
+  @Transactional(readOnly = true)
+  public List<TetherDeposit> getApprovedDepositsForAccountByTetherWallet(String tetherWallet) {
+    return tetherDepositRepository.findByTetherAccount_TetherWalletAndStatus(
+        tetherWallet, TransactionStatus.CONFIRMED);
+  }
 
-    @Override
-    @Transactional
-    public Boolean withdrawAccept(String tetherWallet, BigDecimal amount) {
-        return null;
-    }
+  /** 미승인된 모든 입금내역을 특정 지갑을 기준으로 조회합니다. */
+  @Override
+  @Transactional(readOnly = true)
+  public List<TetherDeposit> getNonApprovedDepositsForAccountByTetherWallet(String tetherWallet) {
+    return tetherDepositRepository.findByTetherAccount_TetherWalletAndStatus(
+        tetherWallet, TransactionStatus.PENDING);
+  }
 
-    /** 모든 입금 내역을 Id를 기준으로 조회합니다. */
-    @Override
-    @Transactional(readOnly = true)
-    public List<TetherDeposit> getDepositsForAccount(Long accountId) {
-        return tetherDepositRepository.findByTetherAccount_IdOrderByRequestedAtDesc(accountId);
-    }
+  /** 특정 지갑의 마지막 입금 내역을 조회합니다. */
+  @Override
+  @Transactional(readOnly = true)
+  public TetherDeposit getLatestDeposit(String tetherWallet) {
+    return tetherDepositRepository
+        .findTopByTetherAccount_TetherWalletOrderByRequestedAtDesc(tetherWallet)
+        .orElseThrow(DepositNotFoundException::new);
+  }
 
-    /** 모든 입금내역을 상태를 기준으로 조회합니다. */
-    @Override
-    @Transactional(readOnly = true)
-    public List<TetherDeposit> getDepositsByStatus(TransactionStatus status) {
-        return tetherDepositRepository.findByStatus(status);
-    }
+  /** 기간을 기준으로 모든 입금 내역을 조회합니다. */
+  @Override
+  @Transactional(readOnly = true)
+  public List<TetherDeposit> getDepositsInRange(LocalDateTime start, LocalDateTime end) {
+    return tetherDepositRepository.findByRequestedAtBetween(start, end);
+  }
 
-    /** 아이디를 기준으로 특정 상태의 입금을 조회합니다. */
-    @Override
-    @Transactional(readOnly = true)
-    public List<TetherDeposit> getApprovedDepositsForAccountById(Long accountId, TransactionStatus status) {
-        return tetherDepositRepository.findByTetherAccount_IdAndStatus(accountId, status);
-    }
+  /** 기간을 기준으로 특정 지갑의 모든 입금 목록을 조회합니다.. */
+  @Override
+  @Transactional(readOnly = true)
+  public List<TetherDeposit> getDepositsInRangeByTetherWallet(
+      String tetherWallet, LocalDateTime start, LocalDateTime end) {
+    return tetherDepositRepository.findByTetherAccount_TetherWalletAndRequestedAtBetween(
+        tetherWallet, start, end);
+  }
 
-    /** 승인된 모든 입금내역을 특정 지갑을 기준으로 조회합니다. */
-    @Override
-    @Transactional(readOnly = true)
-    public List<TetherDeposit> getApprovedDepositsForAccountByTetherWallet(String tetherWallet) {
-        return tetherDepositRepository.findByTetherAccount_TetherWalletAndStatus(tetherWallet, TransactionStatus.CONFIRMED);
-    }
+  /** 상태별 총 입금 합계를 조회합니다. */
+  @Override
+  @Transactional(readOnly = true)
+  public BigDecimal getTotalDepositAmountByStatus(TransactionStatus status) {
+    return tetherDepositRepository.sumByStatus(status.name());
+  }
 
-    /** 미승인된 모든 입금내역을 특정 지갑을 기준으로 조회합니다. */
-    @Override
-    @Transactional(readOnly = true)
-    public List<TetherDeposit> getNonApprovedDepositsForAccountByTetherWallet(String tetherWallet) {
-        return tetherDepositRepository.findByTetherAccount_TetherWalletAndStatus(tetherWallet, TransactionStatus.PENDING);
-    }
-
-    /** 특정 지갑의 마지막 입금 내역을 조회합니다. */
-    @Override
-    @Transactional(readOnly = true)
-    public TetherDeposit getLatestDeposit(String tetherWallet) {
-        return tetherDepositRepository
-                .findTopByTetherAccount_TetherWalletOrderByRequestedAtDesc(tetherWallet)
-                .orElseThrow(DepositNotFoundException::new);
-    }
-
-    /** 기간을 기준으로 모든 입금 내역을 조회합니다. */
-    @Override
-    @Transactional(readOnly = true)
-    public List<TetherDeposit> getDepositsInRange(LocalDateTime start, LocalDateTime end) {
-        return tetherDepositRepository.findByRequestedAtBetween(start, end);
-    }
-
-    /** 기간을 기준으로 특정 지갑의 모든 입금 목록을 조회합니다.. */
-    @Override
-    @Transactional(readOnly = true)
-    public List<TetherDeposit> getDepositsInRangeByTetherWallet(String tetherWallet, LocalDateTime start, LocalDateTime end) {
-        return tetherDepositRepository.findByTetherAccount_TetherWalletAndRequestedAtBetween(tetherWallet, start, end);
-    }
-
-    /** 상태별 총 입금 합계를 조회합니다. */
-    @Override
-    @Transactional(readOnly = true)
-    public BigDecimal getTotalDepositAmountByStatus(TransactionStatus status) {
-        return tetherDepositRepository.sumByStatus(status.name());
-    }
-
-    /** 특정 상태를 기준으로 그 지갑의 모든 입금 총액을 조회합니다. */
-    @Override
-    @Transactional(readOnly = true)
-    public BigDecimal getTotalDepositAmountByStatusAndWallet(TransactionStatus status, String tetherWallet) {
-        return tetherDepositRepository.sumByStatusAndWallet(status.name(), tetherWallet);
-    }
+  /** 특정 상태를 기준으로 그 지갑의 모든 입금 총액을 조회합니다. */
+  @Override
+  @Transactional(readOnly = true)
+  public BigDecimal getTotalDepositAmountByStatusAndWallet(
+      TransactionStatus status, String tetherWallet) {
+    return tetherDepositRepository.sumByStatusAndWallet(status.name(), tetherWallet);
+  }
 }

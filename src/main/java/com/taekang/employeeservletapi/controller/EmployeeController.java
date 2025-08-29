47aaff9ba0aed09a7827288f6f66b0cac818be18
SuniPro <thtnsgus21@gmail.com>
2025-08-29 18:20:56 +0700

    feature : 유저 관리 엔드포인트 추가

--- src/main/java/com/taekang/employeeservletapi/controller/EmployeeController.java
+++ src/main/java/com/taekang/employeeservletapi/controller/EmployeeController.java
@@ -3,36 +3,45 @@ package com.taekang.employeeservletapi.controller;
 import com.taekang.employeeservletapi.DTO.EmployeeDTO;
 import com.taekang.employeeservletapi.DTO.EmployeeUpdateDTO;
 import com.taekang.employeeservletapi.DTO.RegisterRequestDTO;
-import com.taekang.employeeservletapi.entity.employee.Department;
 import com.taekang.employeeservletapi.entity.employee.Employee;
-import com.taekang.employeeservletapi.entity.employee.Level;
 import com.taekang.employeeservletapi.service.EmployeeService;
-import java.util.List;
+import com.taekang.employeeservletapi.service.auth.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("employee")
 public class EmployeeController {
 
+  private static final String MANAGER_ACCESS =
+          "hasAnyAuthority('LEVEL_ADMINISTRATOR','LEVEL_MANAGER', 'LEVEL_OFFICEMANAGER', 'LEVEL_SENIORMANAGER')";
+
   private final EmployeeService employeeService;
+  private final JwtUtil jwtUtil;
 
   @Autowired
-  public EmployeeController(EmployeeService employeeService) {
+  public EmployeeController(EmployeeService employeeService, JwtUtil jwtUtil) {
     this.employeeService = employeeService;
+      this.jwtUtil = jwtUtil;
   }
 
+  @PreAuthorize(MANAGER_ACCESS)
   @PostMapping("create")
-  public ResponseEntity<Employee> createEmployee(
+  public ResponseEntity<Employee> createEmployee(@CookieValue("access-token") String token,
       @RequestBody RegisterRequestDTO registerRequestDTO) {
-    return ResponseEntity.ok().body(employeeService.createEmployee(registerRequestDTO));
+
+    String name = jwtUtil.getEmployeeName(token);
+
+    return ResponseEntity.ok().body(employeeService.createEmployee(name, registerRequestDTO));
   }
 
+  @PreAuthorize(MANAGER_ACCESS)
   @PutMapping("update")
   public ResponseEntity<EmployeeDTO> updateEmployee(
       @RequestBody EmployeeUpdateDTO employeeUpdateDTO) {
@@ -40,63 +49,33 @@ public class EmployeeController {
   }
 
   @GetMapping("get/by/{id}")
-  public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+  public ResponseEntity<EmployeeDTO> getEmployeeById(@PathVariable Long id) {
     return ResponseEntity.ok().body(employeeService.getEmployeeById(id));
   }
 
   @GetMapping("get/by/{name}")
-  public ResponseEntity<Employee> getEmployeeByName(@PathVariable String name) {
+  public ResponseEntity<EmployeeDTO> getEmployeeByName(@PathVariable String name) {
     return ResponseEntity.ok().body(employeeService.getEmployeeByName(name));
   }
 
   @GetMapping("get/all")
-  public ResponseEntity<Page<Employee>> getAllEmployees(
+  public ResponseEntity<Page<EmployeeDTO>> getAllEmployees(
       @PageableDefault(size = 10, sort = "insertDateTime", direction = Sort.Direction.DESC)
           Pageable pageable) {
-    return ResponseEntity.ok().body(employeeService.getAllEmployees(pageable));
+    return ResponseEntity.ok().body(employeeService.getEmployeeList(pageable));
   }
 
-  @GetMapping("get/by/{department}")
-  public ResponseEntity<List<Employee>> getEmployeeListByDepartment(
-      @PathVariable Department department) {
-    return ResponseEntity.ok().body(employeeService.getEmployeeListByDepartment(department));
-  }
-
-  @GetMapping("get/by/{level}")
-  public ResponseEntity<List<Employee>> getEmployeeListByLevel(@PathVariable Level level) {
-    return ResponseEntity.ok().body(employeeService.getEmployeeListByLevel(level));
-  }
-
-  @GetMapping("get/by/{level}/less/then")
-  public ResponseEntity<Page<EmployeeDTO>> getEmployeeListByLevelLessThen(
-      @PathVariable Level level,
-      @PageableDefault(size = 10, sort = "insert_date_time", direction = Sort.Direction.DESC)
-          Pageable pageable) {
-    return ResponseEntity.ok()
-        .body(employeeService.getEmployeeListByLevelLessThen(level, pageable));
-  }
-
-  @GetMapping("get/by/{level}/department/{department}/less/then")
-  public ResponseEntity<Page<EmployeeDTO>> getEmployeeListByLevelAndDepartmentLessThen(
-      @PathVariable Level level,
-      @PathVariable Department department,
-      @PageableDefault(size = 10, sort = "insertDateTime", direction = Sort.Direction.DESC)
-          Pageable pageable) {
-    return ResponseEntity.ok()
-        .body(
-            employeeService.getEmployeeListByLevelAndDepartmentLessThen(
-                level, department, pageable));
-  }
-
-  @GetMapping("get/by/{level}/greater/then")
-  public ResponseEntity<List<EmployeeDTO>> getEmployeeListByLevelGreaterThan(
-      @PathVariable Level level) {
-    return ResponseEntity.ok().body(employeeService.getEmployeeListByLevelGreaterThen(level));
+  @GetMapping("get/all/{site}")
+  public ResponseEntity<Page<EmployeeDTO>> getAllEmployees(
+          @PageableDefault(size = 10, sort = "insertDateTime", direction = Sort.Direction.DESC)
+          Pageable pageable, @PathVariable String site) {
+    return ResponseEntity.ok().body(employeeService.getEmployeeListBySite(site, pageable));
   }
 
   @DeleteMapping("delete/by/{id}")
-  public ResponseEntity<Long> deleteEmployee(@PathVariable Long id) {
-    employeeService.deleteEmployeeById(id);
+  public ResponseEntity<Long> deleteEmployee(@CookieValue("access-token") String token, @PathVariable Long id) {
+    String name = jwtUtil.getEmployeeName(token);
+    employeeService.deleteEmployeeById(name, id);
 
     return ResponseEntity.ok().body(id);
   }

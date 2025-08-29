891ac2e7598ddf94c2eb5db36032bf057d583529
SuniPro <thtnsgus21@gmail.com>
2025-08-30 01:50:07 +0700

    style : 이름 변경(tether -> crypto)

--- src/main/java/com/taekang/employeeservletapi/entity/employee/WorkMenu.java
+++ src/main/java/com/taekang/employeeservletapi/entity/employee/WorkMenu.java
@@ -3,7 +3,7 @@ package com.taekang.employeeservletapi.entity.employee;
 public enum WorkMenu {
   MANAGE_EMPLOYEE,
   REVIEW_EMPLOYEE,
-  MANAGE_TETHER,
-  MANAGE_TETHER_DEPOSIT,
+  MANAGE_CRYPTO,
+  MANAGE_CRYPTO_DEPOSIT,
   NOTIFY
 }

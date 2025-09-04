894a6e35f3d2abbec102278198369552a1630d67
SuniPro <thtnsgus21@gmail.com>
2025-09-04 14:57:11 +0700

    refactor : 불필요 어노테이션 제거

--- src/main/java/com/taekang/employeeservletapi/EmployeeServletApiApplication.java
+++ src/main/java/com/taekang/employeeservletapi/EmployeeServletApiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@Profile("dev")
 public class EmployeeServletApiApplication {
 
   public static void main(String[] args) {
